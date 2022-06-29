3b0435454036acb25a2ef1262b1118b75a246043
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2022-06-30 00:31:44 +0800

    Add Login view controller and view model

---
+++ iOSExamTest/iOSExamTest/Modules/Authentication/Login/UI/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  iOSExamTest
+//
+//  Created by Mark Dennis Diwa on 6/30/22.
+//
+
+import Foundation
+
+class LoginViewModel: LoginViewModelProtocol {}
+
+// MARK: - Method
+
+extension LoginViewModel {
+    func login(using email: String) {
+        
+    }
+}
---
+++ iOSExamTest/iOSExamTest/Modules/Authentication/Login/UI/LoginViewModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModelProtocol.swift
+//  iOSExamTest
+//
+//  Created by Mark Dennis Diwa on 6/30/22.
+//
+
+import Foundation
+
+protocol LoginViewModelProtocol {
+    func login(using email: String)
+}
