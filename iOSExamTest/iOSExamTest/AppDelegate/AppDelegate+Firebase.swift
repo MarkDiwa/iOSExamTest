348f6e25eeb945a6ea2ac042c0b1dcd7d564714a
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2022-06-30 00:39:31 +0800

    Add Firebase Authentication

---
+++ iOSExamTest/iOSExamTest/AppDelegate/AppDelegate+Firebase.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate+Firebase.swift
+//  iOSExamTest
+//
+//  Created by Mark Dennis Diwa on 6/30/22.
+//
+
+import Foundation
+import FirebaseCore
+
+extension AppDelegate {
+    func setupFirebase() {
+        FirebaseApp.configure()
+    }
+}
